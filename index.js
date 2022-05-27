ee0e7c0de3ebde04cc5100e804584b829423a1fd
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-05-27 23:07:29 +0000

    adding basic dark model toggle and function

---
+++ index.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    
+    let myVar= document.getElementById('toggle-button');
+    myVar.style.backgroundColor = "black";
+    myVar.style.color = "white";
+  
+   }
+   
+   
+   

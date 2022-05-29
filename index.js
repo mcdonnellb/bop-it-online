eddd2200e62497925bc710a981a239194806fefd
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-05-29 22:00:58 +0000

    changing game container styling and dark toggle

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+document.getElementsByClassName("fa-solid fa-moon").addEventListener("click", darkMode);
+
 function darkMode() {
+    console.log('Dark Mode is Selected');
+document.getElementById('body').style.color = "white";
+ document.getElementById("body").style.backgroundcolor = "black";
+ 
+      }
+  
+      
+
+
+   function lightMode() {
     
-    let myVar= document.getElementById('toggle-button');
-    myVar.style.backgroundColor = "black";
-    myVar.style.color = "white";
+    let myVar= document.getElementById('sun');
+    let lightModeBody = document.getElementById[0]('body');
+    light-mode-body.style.backgroundColor="red";
+    light-mode-body.style.color;
   
    }
-   
    
    

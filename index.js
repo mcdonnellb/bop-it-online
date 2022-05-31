37fceb3d3f3090ed4fe3eedb685b8cd7fbbf25e0
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-05-31 20:46:51 +0000

    removed audio files I wont be using

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ var pullIt;
 var spinIt;
 let moon = document.getElementById("moon");
 
+/*Setting up the Audio variables for the game*/
+var audio = [];
+audio[0] = new Audio();
+audio[0].src = "assets/audio/Bop.wav";
+audio[1] = new Audio();
+audio[1].src = "assets/audio/Twist.wav";
+audio[2] = new Audio();
+audio[2].src = "assets/audio/Spin.wav";
+audio[3] = new Audio();
+audio[3] = "assets/audio/Pull.wav";
+audio[4] = new Audio();
+audio[4] = "assets/audio/Flick";
+
+
+
 
 /* document.getElementsByClassName("fa-solid fa-moon").addEventListener("click", darkMode);
 */
@@ -19,10 +34,9 @@ document.getElementById('body').style.color = "white";
  
       }
   */
-         
-
+      let var element = document.body;
       function myFunction() {
-        var element = document.body;
+    
         element.classList.toggle("dark-mode");
       }
 

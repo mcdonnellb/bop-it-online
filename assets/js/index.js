af33f6390010ddcbf03fea73452e05acb855bd74
mcdonnellb <bernadettemichelemcdonnell@gmail.com>
2022-06-13 16:44:43 +0000

    changing var types to const

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,21 @@
 /* setting variables for the game */
-var score = 0 ;
-//var scoreboard = document.getElementByID("scoreboard").innerHTML= score;
+let score = 0 ;
 
-//var items = ["bop-It","twist-It","flick-It","pull-it","spin-It"];
+
+//const items = ["bop-It","twist-It","flick-It","pull-it","spin-It"];
 //just leaving the bop it and twist it instruction in the array for now for testing purposes
-var items = ["bop-it", "twist-it"];
-
-var randomItems = items[Math.floor(Math.random()* items.length)];
-var ci= document.getElementById("computer-instructions").innerHTML;
+const items = ["bop-it", "twist-it"];
+const namePlate = document.getElementById("name");
 
 
-var bopItButton = document.getElementById("bop-it");
+const randomItems = items[Math.floor(Math.random()* items.length)];
+let ci= document.getElementById("computer-instructions").innerHTML;
+
+
+const bopItButton = document.getElementById("bop-it");
 bopItButton.addEventListener("click", bopItClicked);
 
-twistItButton = document.getElementById("twist-it");
+const twistItButton = document.getElementById("twist-it");
 twistItButton.addEventListener("click", twistItClicked);
 //bopItButton.addEventListener("click", twistItClicked);
 //var flickItButton = document.getElementById("flick-it");
@@ -31,8 +33,7 @@ var audioFiles= (['Pull.wav', 'Flick.wav', 'Spin.wav']);
 var randomAudio = Math.floor * (Math.random * audioFiles)  
 var bopItAudio = new Audio();
 bopItAudio.src = "assets/audio/Bop.wav";
-randomAudio.src= (["assets/audio/Bop.wav","assets/audio/Flick.wav","assets/audio/Twist.wav"]);
-playRandom = Math.floor * (Math.random * randomAudio.src);
+
 
 
 
@@ -49,25 +50,25 @@ element.classList.toggle("dark-mode");
 
 
     // Function for Start Game - this needs to Welcome the User, count down from 5 then select from the array, will keep going as user selects the right button but ends when they select the wrong button then displays Game over screen
-    var startButton = document.getElementById("startButton");
-    startButton.addEventListener("click", startGame);
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", startGame);
 
-  function startGame() {
-      var scoreBoard = document.getElementById("scoreboard");  
-      var score =0;
-  
-      // choose the computer instruction from the Array
-     let computerInstructions= document.getElementById("computer-instructions").innerHTML= randomItems;
-     var timeleft = 4;
-     var downloadTimer = setInterval(function(){
-     if(timeleft <= 0){
-     clearInterval(downloadTimer);
-    //alert("GAME OVER- TOO SLOW BRO" +score);
-  }
+function startGame() {
+  var scoreBoard = document.getElementById("scoreboard");  
+  var score =0;
+
+  // choose the computer instruction from the Array
+ // let computerInstructions= document.getElementById("computer-instructions").innerHTML= randomItems;
+ generateComputerInstructions();
+  var timeleft = 4;
+  var downloadTimer = setInterval(function() {
+    if(timeleft <= 0){
+      clearInterval(downloadTimer);
+      //alert("GAME OVER- TOO SLOW BRO" +score);
+    }
     document.getElementById("progressBar").value = 4 - timeleft;
     timeleft -= 1;
-    }, 1000);
-    
+  }, 1000);
 }
     
    //This is for the user to input their name it will add a message for them to the screen//       
@@ -75,7 +76,7 @@ document.getElementById("user-details").addEventListener("submit", nameEntered);
 
     function nameEntered(event) {
     event.preventDefault();
-let namePlate = document.getElementById("name");
+
 document.getElementById("game-title").innerHTML= "This is Your Time to Shine " + namePlate.value +  " playing Bop It Extreme Online";
     }
 
@@ -106,16 +107,16 @@ score +=1;
 //clearInterval(downloadTimer);
 document.getElementById("progressBar").value = 4 - timeleft;
 timeleft -= 1;
-//alert(score);
+
 console.log(score);
-//alert("YOU GOT ANOTHER POINT WOOHOO")
+
 console.log('You got another point WOOHOO');
-document.getElementById('scoreboard').innerHTML = gamename +"Score" + score;
+let scoreBUpdate = document.getElementById('scoreboard').innerHTML = namePlate +"Score" + score;
 
 
     } else {
   //    alert('GAME OVER- You lose!!');
-  console.log('Game over-you lose!);
+  console.log('Game over-you lose!');
     }
 
 
@@ -127,9 +128,6 @@ bopItAudio.play();
   function twistItClicked(){
     clickedTwistIT = this.id;
     ci = document.getElementById("computer-instructions").innerHTML;
-  //  alert ("You clicked twist it");
-    //alert(clickedTwistIT);
-
     if (ci == clickedTwistIT){
       score +=1;
       //alert(score);
@@ -137,7 +135,7 @@ bopItAudio.play();
       document.getElementById('scoreboard').innerHTML = score;
       //alert("YOU GOT ANOTHER POINT WOOHOO");
       console.log('you got another point WOOHOO');
-      doucument.getElementByID("scoreboard").value= gamename + score;
+      document.getElementByID("scoreboard").value= namePlate+ score;
       document.getElementById("progressBar").value = 4 - timeleft;
       timeleft += 3;
  
@@ -164,11 +162,21 @@ document.getElementById("show-rules-section").innerHTML ="The rules of the game 
 
 // function for the timer- counts down from 4 seconds 
 // this needs to be tied in with the start game funtion so that when the random selection form the array is returned, the user has 4 seconds to choose the correct button or the end game screen is displayed
-//var timeleft = 4;
-//var downloadTimer = setInterval(function(){
-  //if(timeleft <= 0){
-   // clearInterval(downloadTimer);
-  //}
-  //document.getElementById("progressBar").value = 4 - timeleft;
-  //timeleft -= 1;
-//}, 1000);
+var timeleft = 4;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+  document.getElementById("progressBar").value = 4 - timeleft;
+  timeleft -= 1;
+}, 1000);
+
+
+
+// function to deal with generating the random selection from the array
+
+function generateComputerInstructions() {
+  let computerInstructions= document.getElementById("computer-instructions").innerHTML= randomItems;
+  return comupterInstructions;
+
+}
